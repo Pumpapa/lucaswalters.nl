1dec5dcd20c8bea03e155a852d157eb500fb1d75
Pum Walters <pum@babelfish.nl>
2015-12-03 23:14:42 +0100

    initial commit

---
+++ cache/compiled/files/2efdbdf694fe05d3b7d51bfac49909dc.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/pum/Dropbox/Sloth/Sites/MacWebsitesDocuments/lucaswalters.nl/user/config/security.yaml',
+    'modified' => 1449180877,
+    'data' => [
+        'salt' => '0ufqKAW6p4QRYz'
+    ]
+];
